b74a6dd5a5b8de08cbca0637bd0f382c366d46ff
Gabriel Dantas <gabrielnsdantas@gmail.com>
2023-05-07 18:36:34 -0300

    fix response size

--- server.cpp
+++ server.cpp
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define MAX_SIZE 1024
+
 int main()
 {
     int server_sockfd, client_sockfd;
@@ -34,20 +36,21 @@ int main()
 
     while (1)
     {
-        int valor;
+        char buffer[MAX_SIZE];
 
         printf("Servidor esperando...\n");
 
         client_len = sizeof(client_address);
 
-        ssize_t receiveResult = recvfrom(server_sockfd, &valor, sizeof(valor), 0, (struct sockaddr *)&client_address, &client_len);
+        ssize_t receiveResult = recvfrom(server_sockfd, &buffer, sizeof(buffer), 0, (struct sockaddr *)&client_address, &client_len);
         if (receiveResult < 0)
         {
             printf("erro no recvfrom");
             exit(1);
         }
+        buffer[receiveResult] = '\0';
 
-        ssize_t sendResult = sendto(server_sockfd, &valor, sizeof(valor), 0, (struct sockaddr *)&client_address, client_len);
+        ssize_t sendResult = sendto(server_sockfd, &buffer, receiveResult, 0, (struct sockaddr *)&client_address, client_len);
         if (sendResult < 0)
         {
             printf("erro no sendto");
